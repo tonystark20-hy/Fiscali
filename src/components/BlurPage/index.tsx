eea10977d36b939fee7a2cd639e847b27a9c8728
tonystark20-hy <39941618+tonystark20-hy@users.noreply.github.com>
2024-06-18 09:41:02 -0700

    login

--- src/components/BlurPage/index.tsx
+++ src/components/BlurPage/index.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './index.css'; // Make sure to create and import the CSS file
+import { useNavigate } from "react-router-dom";
+import { Text, Img, Heading, Button, SelectBox } from "../../components";
+
 
 const BlurPage = () => {
   const [isTokenValid, setIsTokenValid] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Example: Check for a token stored in local storage (you can adjust this logic)
     const token = localStorage.getItem('authToken');
+    
 
     // Function to check if the token is valid (you can replace this with your own validation logic)
     const isValidToken = (token) => {
@@ -18,13 +23,25 @@ const BlurPage = () => {
     setIsTokenValid(isValidToken(token));
   }, []);
 
+  const navigateLogin = () => {
+    // Navigate to the '/reviewcovenantmatches' route
+    navigate("/login");
+  };
+
   return (
     <>
       {!isTokenValid && (
-        <div id="blur-cover">
-          <div className="message">
-            Access Denied
+        <div id="blur-cover" className="flex flex-col">
+          <div className="flex">
+            <div className="message">
+              Access Denied
+            </div>
           </div>
+          <Button
+            className="flex whitespace-nowrap items-center justify-center h-[39px]  px-[35px]  text-white-A700_01 text-center text-base font-medium bg-indigo-800 rounded-[3px]"
+            onClick={navigateLogin}
+          >Login here
+          </Button>
         </div>
       )}
     </>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,18 +4,27 @@ import "./index.css";
 import { CloseSVG } from "../../assets/images";
 import { Input } from "../../components/Input";
 import { MenuItem, Menu, Sidebar } from "react-pro-sidebar";
-import "./index.css";
+import { useNavigate } from "react-router-dom";
+
 
 
 const Header = () => {
     const [searchBarValue, setSearchBarValue] = React.useState("");
     const [collapsed, setCollapsed] = React.useState(false);
+    const navigate = useNavigate();
+
+    const navigateUpload = () => {
+      // Navigate to the '/reviewcovenantmatches' route
+      navigate("/");
+    };
 
   return (
     <header className="flex justify-center items-center w-full shadow-lg h-24 md:h-fit mb-2">
     <div className="flex w-[100%] md:w-full  ">
       <Img
         src="images/img_image_23.png"
+        onClick={()=>navigateUpload()}
+        style={{cursor: "pointer"}}
         className="h-[60px] w-[200px] md:h-auto md:w-auto object-cover"
       />
     </div>
